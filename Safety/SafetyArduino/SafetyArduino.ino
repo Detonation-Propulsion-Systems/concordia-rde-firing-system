f3cf51f1745388ba61ef2e3a4c79026d5c443d08
seanfcb <38043177+seanfcb@users.noreply.github.com>
2019-11-07 13:25:33 -0500

    Add files via upload

--- Safety/SafetyArduino/SafetyArduino.ino
+++ Safety/SafetyArduino/SafetyArduino.ino
@@ -3,7 +3,7 @@ int reset        = 3;
 int warninglight = 11;
 int go           = 12; //Defines all used ports
 
-int Pmax         = 25; //Maximum desired blast chamber pressure, in psi
+int Pmax         = 30; //Maximum desired blast chamber pressure, in psi
 int sensor       = 30;//Max pressure reading from transducer, in psi
 double offset    = 0.00;//Offset to read atmospheric. in V Recalibrate for new transducer
 //offset values
@@ -14,7 +14,7 @@ double voltage;
 double psi;
 
 void setup() {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   pinMode(reset,INPUT);
   pinMode(warninglight,OUTPUT);
   pinMode(go,OUTPUT);
@@ -27,15 +27,15 @@ digitalWrite(warninglight, LOW);
 rawdata = analogRead(datainput);
 voltage = 5*rawdata/1023+offset;
 psi     = 0.25*(voltage-0.5)*sensor;
-Serial.println(psi);
-delay(500);
+//Serial.println(psi);
+//delay(500);
 if (psi>Pmax){
   
   digitalWrite(go, HIGH);
   digitalWrite(warninglight, HIGH);
-  Serial.println("MAX CHAMBER PRESSURE EXCEEDED");
-  Serial.println("CHAMBER MAY BE FILLED WITH UNBURNED FUEL. USE CAUTION TO VENT CHAMBER.");
-  Serial.println("PRESS RESET BUTTON WHEN CHAMBER IS SAFE AND DEPRESSURIZED");
+  //Serial.println("MAX CHAMBER PRESSURE EXCEEDED");
+  //Serial.println("CHAMBER MAY BE FILLED WITH UNBURNED FUEL. USE CAUTION TO VENT CHAMBER.");
+  //Serial.println("PRESS RESET BUTTON WHEN CHAMBER IS SAFE AND DEPRESSURIZED");
   while (digitalRead(reset)==LOW){
     
   } 
