92e2a0b11500f86f2b061933aa2739603031a86d
Charles Basenga Kiyanda <personal@charleskiyanda.com>
2023-01-23 00:14:02 -0500

    Firing controller
    * adding support for CO2 flush (RDE facility)

--- timing/BubbleMachine/BubbleMachine.ino
+++ timing/BubbleMachine/BubbleMachine.ino
@@ -3,6 +3,7 @@ int yelLed = 12;
 int redLed = 11;
 int valvePin = 10; //Actuator signal pin.
 int bubblePin = 9; //Ignition signal pin.
+int CO2Pin    = 7; //N.O. solenoid for CO2 flow
 
 //Trigger remote pins
 
@@ -11,27 +12,32 @@ int rewindPin = 4;
 int trigger = 3;
 
 bool checkGreen = LOW;
-bool checkYel = LOW;
-bool checkRed = LOW;
+bool checkYel   = LOW;
+bool checkRed   = LOW;
 
-int buttonDelay = 250; //Time you have to press a button on the remote for an action to ensue.
-int triggerTime = 10000; //Trigger length.
-int ignitionTime = 5000; //Ignition delay after the start of the trigger.
+int buttonDelay  = 250; //Time you have to press a button on the remote for an action to ensue.
+int triggerTime  = 2000; //Trigger length i.e. length of actuation of the valves.
+int ignitionTime = 1000; //Ignition delay after the start of the trigger.
+int CO2InjTime   = 500; //Duration CO2 is flushed to inert the sandworm environment
 
 unsigned long startTime; //Time associated to the reading of the millis() function when the test starts.
 unsigned long runTime; //Placeholder for the millis() function to use for comparison with trigger and ignition time.
 
 void setup() {
 
-  pinMode(greenLed, OUTPUT);
-  pinMode(yelLed, OUTPUT);
-  pinMode(redLed, OUTPUT);
+  pinMode(greenLed,  OUTPUT);
+  pinMode(yelLed,    OUTPUT);
+  pinMode(redLed,    OUTPUT);
   pinMode(bubblePin, OUTPUT);
-  pinMode(valvePin, OUTPUT);
+  pinMode(valvePin,  OUTPUT);
+  pinMode(CO2Pin,    OUTPUT);
 
   pinMode(forwardPin, INPUT);
-  pinMode(rewindPin, INPUT);
-  pinMode(trigger, INPUT);
+  pinMode(rewindPin,  INPUT);
+  pinMode(trigger,    INPUT);
+
+  PORTB = B00100100;
+  digitalWrite(CO2Pin,HIGH);
 }
 
 //LED colour code
@@ -48,6 +54,10 @@ void unexpectedTrigger() { //Freezes the timing Arduino until the trigger is swi
 
 void manualAbort() { //Disarms the system by resetting all control pins to their initial, safe state.
 
+  digitalWrite(CO2Pin,HIGH);
+  delay(CO2InjTime);
+  digitalWrite(CO2Pin,LOW);
+  
   PORTB = B00100100;
 
   checkRed = LOW;
@@ -58,7 +68,9 @@ void manualAbort() { //Disarms the system by resetting all control pins to their
 void loop() {
 
   PORTB = B00100100;
-
+  digitalWrite(CO2Pin,HIGH);
+  delay(1000);
+  digitalWrite(CO2Pin,LOW);
   while (digitalRead(trigger) == HIGH) { //Timing Arduino can't advance if the trigger is on.
 
     unexpectedTrigger();
